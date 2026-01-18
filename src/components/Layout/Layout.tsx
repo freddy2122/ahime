d0ebe196392c462a2b295d27c9e469856ecf345e
freddy2122 <akoutadeo@gmail.com>
2026-01-18 04:04:56 +0100

    fix: Update Layout to support both children and Outlet for nested routes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react'
+import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import MobileBottomNav from './MobileBottomNav'
 
 interface LayoutProps {
-  children: ReactNode
+  children?: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
@@ -12,7 +13,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="flex flex-col min-h-screen overflow-x-hidden w-full">
       <Navbar />
       <main className="flex-grow pt-20 pb-16 lg:pb-0 w-full overflow-x-hidden">
-        {children}
+        {children || <Outlet />}
       </main>
       <Footer />
       <MobileBottomNav />
